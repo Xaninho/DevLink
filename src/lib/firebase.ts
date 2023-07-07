697a55131fcb35e7922101157fd157475cdcf81e
Alexandre Neves <alexandre.neves@garland.pt>
2023-07-07 17:33:53 +0100

    🥤 Firebase Auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from './env-config';
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
     apiKey: API_KEY,
@@ -22,3 +23,32 @@ export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+    let unsubscribe: () => void;
+  
+    if (!auth || !globalThis.window) {
+      console.warn('Auth is not initialized or not in browser');
+      const { subscribe } = writable<User | null>(null);
+      return {
+        subscribe,
+      }
+    }
+  
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+      unsubscribe = onAuthStateChanged(auth, (user) => {
+        set(user);
+      });
+  
+      return () => unsubscribe();
+    });
+  
+    return {
+      subscribe,
+    };
+  }
+  
+  export const user = userStore();
